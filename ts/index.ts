61993c4dd8694363949ccf10a748d89839c3c2e6
iMasanari <14dd023@gmail.com>
2016-11-27 09:06:20 +0900

    データ部分を分離

--- ts/index.ts
+++ ts/index.ts
@@ -5,35 +5,8 @@ interface PageData {
     image: string
     sound: string
 }
-const pageDataList: PageData[][] = [[
-    { image: "pic/01.png", sound: "sound/01.mp3" },
-    { image: "pic/02.png", sound: "sound/02.mp3" },
-    { image: "pic/03.gif", sound: "sound/03.mp3" },
-    { image: "pic/04.png", sound: "sound/04.mp3" },
-    { image: "pic/05.png", sound: "sound/00.mp3" },
-    { image: "pic/06.png", sound: "sound/00.mp3" },
-    { image: "pic/07.png", sound: "sound/05.mp3" },
-    { image: "pic/08.png", sound: "sound/05.mp3" },
-    { image: "pic/09.png", sound: "sound/06.mp3" },
-    { image: "pic/10.png", sound: "sound/07.mp3" },
-    { image: "pic/11.png", sound: "sound/08.mp3" },
-    { image: "pic/12.png", sound: "sound/09.mp3" },
-    { image: "pic/13.png", sound: "sound/09.mp3" }
-], [
-    { image: "pic/14.png", sound: "./sound/00.mp3" },
-    { image: "pic/15.png", sound: "./sound/10.mp3" },
-    { image: "pic/16.png", sound: "./sound/11.mp3" },
-    { image: "pic/17.png", sound: "./sound/12.mp3" },
-    { image: "pic/18.png", sound: "./sound/13.mp3" },
-    { image: "pic/19.png", sound: "./sound/14.mp3" },
-    { image: "pic/20.png", sound: "./sound/15.mp3" },
-    { image: "pic/21.png", sound: "./sound/15.mp3" },
-    { image: "pic/22.png", sound: "./sound/15.mp3" },
-    { image: "pic/23.png", sound: "./sound/16.mp3" },
-    { image: "pic/24.png", sound: "./sound/17.mp3" },
-    { image: "pic/25.png", sound: "./sound/18.mp3" },
-    { image: "pic/26.png", sound: "./sound/19.mp3" },
-]]
+
+declare const pageDataList: PageData[][]
 
 const topPage = document.getElementById('top')
 const comic = document.getElementById('comic')
