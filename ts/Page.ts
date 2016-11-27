773f45ac30c5244570a9a6aa7e512fbc32dc8b77
iMasanari <14dd023@gmail.com>
2016-11-27 16:26:10 +0900

    画面比率による表示の調整

--- ts/Page.ts
+++ ts/Page.ts
@@ -1,5 +1,7 @@
 class Page {
-    pageList: { el: HTMLElement, visit: Function, y?: number }[] = []
+    pageList: { el: HTMLElement, image: HTMLImageElement, visit: Function, y?: number }[] = []
+    active = 0
+    private resizeTimer: number
 
     constructor(pageList: { url: string, visit: Function }[], callback?: Function) {
         let loadingCounter = 0
@@ -17,8 +19,9 @@ class Page {
     load(url: { url: string, visit: Function }, callback: Function) {
         const image = new Image()
         const el = document.createElement('div')
-        const data = { el, visit: url.visit }
+        const data = { el, image, visit: url.visit }
 
+        el.className = 'page'
         this.pageList.push(data)
 
         image.onload = () => {
@@ -33,38 +36,46 @@ class Page {
             el.appendChild(page.el)
         })
 
-        this.pageList.forEach(page => {
-            page.y = page.el.offsetTop
-        })
+        this.resize()
 
         window.addEventListener('scroll', this.scroll, false)
+        window.addEventListener('resize', this.delayResize, false)
     }
     remove() {
         window.removeEventListener('scroll', this.scroll, false)
+        window.addEventListener('resize', this.delayResize, false)
 
         this.pageList.forEach(({el}) => {
             if (el.parentNode) el.parentNode.removeChild(el)
         })
     }
-    scroll = (() => {
-            let active = 0
+    scroll = () => {
+        const scrollTop = (document.documentElement.scrollTop || document.body.scrollTop)
+        const border = scrollTop + window.innerHeight * 0.8
 
-            return () => {
-                const scrollY = (document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight;
+        for (let i = this.pageList.length; i--;) {
+            const val = this.pageList[i].y;
 
-                for (let i = this.pageList.length; i--;) {
-                    const val = this.pageList[i].y;
-
-                    if (val <= scrollY) {
-                        if (active !== val) {
-                            active = val
-                            const page = this.pageList[i]
-                            page.visit()
-                        }
-
-                        return;
-                    }
+            if (val < border) {
+                if (this.active !== val) {
+                    this.active = val
+                    const page = this.pageList[i]
+                    page.visit()
                 }
+
+                return;
             }
-        })()
+        }
+    }
+    resize = () => {
+        console.log('resize');
+
+        this.pageList.forEach(page => {
+            page.y = page.image.offsetTop
+        })
+    }
+    delayResize = () => {
+        clearTimeout(this.resizeTimer)
+        this.resizeTimer = setTimeout(this.resize, 200)
+    }
 }
